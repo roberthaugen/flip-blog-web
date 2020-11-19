643a467f8f0169172b28c04a3741ffd2c5309983
Robert Haugen <robert@haugen.me>
2020-11-19 18:30:11 +0100

    update authorinfo

--- components/AuthorIntro.js
+++ components/AuthorIntro.js
@@ -10,14 +10,14 @@ const AuthorIntro = () =>
               width={64}
               height={64}
               className="mr-3"
-              src="https://avatars1.githubusercontent.com/u/9482724?s=460&u=69a6acab13fd5547a4e316e496b573271077147f&v=4"
+              src="https://avatars1.githubusercontent.com/u/3786139?s=460&u=2f6e568496dcd2402736a9544ddc4715b2c3ba29&v=4"
               alt="Generic placeholder"
             />
             <Media.Body>
               <h5 className="font-weight-bold mb-0">Hello Earls,</h5>
               <p className="welcome-text">
-              My name is Flip Jerga and I am an experienced software engineer and freelance developer.
-              and this is my blog page.
+              My name is Robert Haugen and I am an unexperienced software engineer and freelance developer.
+              and this is my demo blog page.
               </p>
             </Media.Body>
           </Media>
